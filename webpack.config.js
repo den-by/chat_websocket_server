3c7c51017e46a0148e81817448e385642d9af992
Denis <denis.da.by@gmail.com>
2020-01-08 00:26:27 +0300

    1

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const paths = {
+    src: path.resolve(__dirname, 'src'),
+    dist: path.resolve(__dirname, 'dist')
+};
+
+module.exports = {
+    watch: true,
+    devServer: {
+        contentBase: path.join(__dirname, 'dist')
+    },
+    context: paths.src, // базовая директория для точек входа и загрузчиков
+    entry: {
+        app: './index'  // точка входа в приложение, наш src/index.ts файл, названием итогового бандла будет имя свойства - app
+    },
+
+    output: {
+        path: paths.dist,  // путь для результатов сборки
+        filename: '[name].bundle.js'  // название итогового бандла, получится dist/app.bundle.js
+    },
+
+    resolve: {
+        extensions: [".wasm", ".mjs", ".js", ".jsx", ".ts", ".tsx", ".json"],
+    },
+
+    devtool: 'inline-source-map', // дополнительные настройки и загрузчики не требуются, хотя даже официальный рецепт от TypeScript рекомендует source-map-loader и поле в tsconfig - "sourceMap": true
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'awesome-typescript-loader'
+            } // загрузчик для обработки файлов с расширением .ts
+        ]
+    }
+};
